a49256f7766076fc911823999af207a44740ea5b
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-10 11:48:11 +0700

    mengecek banyak element

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -38,20 +38,19 @@ public:
         }
         queue_array[REAR] = num; 
         }
-
-   
-   
-   
-   
-   
+         void remove() {
+     // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
         // cek jika antrian hanya memiliki satu elemen
-        if (FRONT == REAR) {
-            FRONT = -1;
-            REAR =-1;
-        }
+       
+       
+       
+       
         else {
             // jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
             if (FRONT == max -1)
