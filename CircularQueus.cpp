da990ae9ca86dd29cd4ee8571cc72baa54ba7f34
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-10 11:45:47 +0700

    membuat remove antrian

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -30,20 +30,20 @@ public:
         REAR = 0;
         }
     else {
-        
-        
-        
-        
-        
-        
-        
-    }
+         // jika REAR berada di posisi terakhir array, kembali ke array awal
+        if (REAR == max - 1)
+        REAR = 0;
+        else
+        REAR = REAR + 1;
+        }
+        queue_array[REAR] = num; 
+        }
 
-    void remove() {
-        // cek apakah antrian kosong
-        if (FRONT == -1) {
-            cout << "Queue underflow\n";
-            return;
+   
+   
+   
+   
+   
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
