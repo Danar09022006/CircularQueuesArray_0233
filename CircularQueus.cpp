24e8dc2057e1c03bfc9e13b690990ca5be4c7d09
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-10 11:12:49 +0700

    class public

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -1,13 +1,14 @@
 #include <iosteram>
 using namespace std;
 
+Class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
 
 
 
-public:
-    Queues() {
-        FRONT = -1;
-        REAR = -1;
+
+
     }
 
     void insert() {
