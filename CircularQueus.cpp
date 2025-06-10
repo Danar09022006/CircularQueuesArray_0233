aa4ff5364bccd51ec66152b8064947af0e73f564
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-10 11:11:47 +0700

    membuat class queues

---
+++ CircularQueus.cpp
@@ -0,0 +1,91 @@
+#include <iosteram>
+using namespace std;
+
+
+
+
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT =0;
+            REAR = 0;
+        }
+        else {
+         // jika REAR berada di posisi terakhir array, kembali ke array awal
+        if (REAR == max - 1)
+            REAR = 0;
+        else
+            REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    void remove() {
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR =-1;
+        }
+        else {
+            // jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max -1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+
+    void display() {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+        
+        // jika FRONT_position == REAR_position, literasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        else {
+            // jika FRONT_position > REAR_position, literasi dari FRONT_position hingga akhir array
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
